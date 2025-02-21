11921be006e0f9c402be0619228e03be7da9c4ac
sanandreas11 <andreaandonis@outlook.com>
2025-02-21 09:47:51 +0100

    makes the color switch work correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const createCells = function () {
-  for (i = 0; i < 76; i++) {
+  for (let i = 0; i < 76; i++) {
     const cell = document.createElement("div") //<div><div/>
     cell.classList.add("borders") //<div class=borders><div/>
     const cellValue = document.createElement("h3") //<h3><h3/>
@@ -13,13 +13,14 @@ createCells()
 
 const numberExtraction = function () {
   const result = document.getElementById("result")
-  const extraction = Math.ceil(Math.random() * 77)
+  const extraction = Math.ceil(Math.random() * 76)
   result.innerText = extraction
   return extraction
 }
 const highlight = function () {
-  for (i = 0; i < 76; i++) {
-    if (numberExtraction() === i + 1) {
+  const random = numberExtraction()
+  for (let i = 0; i < 76; i++) {
+    if (random === i + 1) {
       const cell = document.querySelectorAll("main div")[i]
       cell.classList.add("selected")
     }
@@ -30,7 +31,7 @@ const playerCells = function (e) {
   e.preventDefault()
   playerNumber = document.getElementsByClassName("values")[i]
   console.log(playerNumber)
-  for (i = 0; i < 24; i++) {
+  for (let i = 0; i < 24; i++) {
     const cell = document.createElement("div") //<div><div/>
     cell.classList.add("borders") //<div class=borders><div/>
     const cellValue = document.createElement("h3") //<h3><h3/>
